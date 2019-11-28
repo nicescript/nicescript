c9c5f6429d60188510a6239aa30007b0ec16dce9
Sergey Kashulin <sergey@sergey-TM1701>
2019-11-28 15:37:41 +0300

    .not.

--- core/expect.js
+++ core/expect.js
@@ -1,14 +1,14 @@
 def(nice, 'expectPrototype', {});
 
-const toString = v => JSON.stringify(v);
+const toString = v => v.toString ? v.toString() : JSON.stringify(v);
 
 reflect.on('Check', f => {
   f.name && def(nice.expectPrototype, f.name, function(...a){
     this.value && this.value._compute && this.value._compute();
-    const res = f(this.value, ...a);
+    const res = this._preF ? this._preF(f(this.value, ...a)) : f(this.value, ...a);
     if(!res || (res && res._isAnything && res._type === nice.Err)){
       const e = new Error(this.text || ['Expected', toString(this.value),
-            'to be', f.name, ...a.map(toString)].join(' '));
+        this._preMessage || '', 'to be', f.name, ...a.map(toString)].join(' '));
       e.shift = 1;
       throw e;
     }
@@ -23,6 +23,12 @@ def(nice, function expect(value, ...texts){
 
 defGet(nice.expectPrototype, function text(){
   return nice.format(...this.texts);
+});
+
+defGet(nice.expectPrototype, function not(){
+  this._preF = v => !v;
+  this._preMessage = 'not';
+  return this;
 });
 
 def(nice.expectPrototype, function message(...a){
